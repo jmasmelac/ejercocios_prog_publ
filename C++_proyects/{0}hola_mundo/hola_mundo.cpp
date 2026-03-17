77085520da323d71889bc2750e60cb9e0600a6cf
jmasmelac <jmasmelac@hotmail.com>
2026-03-16 20:32:29 -0500

    anexion ejercicios de c++ pero bien

---
+++ C++_proyects/{0}hola_mundo/hola_mundo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main(){
+    std::cout << "\nHola Mundo!\n";
+    //el "<< std::endl" es para hacer un salto de linea
+    std::cout << "que anda haciendo?\n"<< std::endl;
+    system("pause");
+    return 0;
+    /*
+    los comentarios de 
+    varias lineas son
+    con estos caracteres.
+    */
+}
